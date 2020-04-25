34d229bb5dd5645b2ae6cce74e99f0e45ba37415
David Keller <d.keller@oodrive.com>
2020-04-25 13:30:17 +0200

    add keycloak loginUrl as global variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
@@ -7,6 +7,11 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  @ViewChild('loginFormHTMLElement', {
+    read : ElementRef,
+    static : true,
+  }) loginFormHTMLElement: ElementRef;
+
   loginForm = this.fb.group({
     username: [null, Validators.required],
     password: [null, Validators.required],
@@ -16,7 +21,13 @@ export class LoginComponent {
 
   onSubmit() {
     if (this.loginForm.valid) {
-      alert('do login!');
+      const htmlFormElement = this.loginFormHTMLElement.nativeElement;
+      let actionUrl = (window as { [key: string]: any })["g_urlLoginAction"] as string;
+      actionUrl = actionUrl.replace( /&amp;/g, '&');
+      htmlFormElement.action = actionUrl;
+      console.log('g_urlLoginAction: ', actionUrl);
+
+      //el.submit();
     }
   }
 }
