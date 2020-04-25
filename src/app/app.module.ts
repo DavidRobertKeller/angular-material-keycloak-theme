c145112c8c9384cac13681914879cc716ca46905
David Keller <d.keller@oodrive.com>
2020-04-25 16:39:15 +0200

    update build conf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
@@ -12,20 +10,18 @@ import { MatCardModule } from '@angular/material/card';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    component: LoginComponent,
-  }
-];
+// import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
-    RouterModule.forRoot(appRoutes),
+    // AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
@@ -33,7 +29,8 @@ const appRoutes: Routes = [
     MatSelectModule,
     MatRadioModule,
     MatCardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
